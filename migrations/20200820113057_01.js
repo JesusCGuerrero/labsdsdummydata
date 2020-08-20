1b7b10decaf89a5c96ae0faba129dbabde3fa6c8
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-08-20 11:31:55 -0400

    aggain

---
+++ migrations/20200820113057_01.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema
+
+    .createTable('ds_string', ds_string => {
+        ds_string.increments();
+        ds_string.string('data')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('ds_string')
+};
